7f1784fff187b6125aa4186917559add1b8a27c0
Chawn Neal <chawnneal@gmail.com>
2026-05-18 13:04:07 -0400

    implment basic Category test

--- backend/store/tests.py
+++ backend/store/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Category
 
 # Create your tests here.
+class CategoryModelTest(TestCase):
+  def test_category_model_exists(self):
+    category = Category.objects.count()
+
+    self.assertEqual(category, 0)
