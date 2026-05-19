65c6fbbbf132f6ac6dcf92f940ade01af3d6ca4b
Chawn Neal <chawnneal@gmail.com>
2026-05-19 19:29:01 -0400

    implement tailwind css and projects list ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [message, setMessage ] = useState('');
+  const [products, setProducts] = useState(['']);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/api')
+    fetch('http://127.0.0.1:8000/api/products/')
     .then(response => response.json())
-    .then(data => setMessage(data.message))
+    .then(data => setProducts(data))
     .catch(error => console.error('Error fetching message',error))
   },[]);
 
   return (
-    <div>
-      <h1>Message from Backend:</h1>
-      <p>{message || 'Loading...'}</p>
+    <div className='min-h-sceen bg-gray-100 test-gray-800'>
+      <h1 className='text-3xl font-bold underline'>Product List</h1>
+      <div className='container mx-auto p-4'>
+        {products.map(product => (
+          <div key={product.id} className='bg-white p-4 rounded shadow mb-4'>
+            <h2 className='text-xl font-semibold'>{product.name}</h2>
+            <p className='text-gray-600'>{product.description}</p>
+            <p className='text-gray-600 font-bold'>${product.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 
