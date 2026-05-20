854e320c255ab522643527cdb8a1bb9411b46c7c
Chawn Neal <chawnneal@gmail.com>
2026-05-20 00:55:00 -0400

    impliment React components for ProductList and ProductCard

---
+++ frontend/components/ProductCard.jsx
@@ -0,0 +1,15 @@
+function ProductCard({product}) {
+  return (
+    <div className="bg-white rounded-xl shadow-md hover:shadow-lg">
+      <img
+      src = {product.image}
+      alt = {product.name}
+      className="w-full h-56 object-cover rounded-lg mb-4"
+      />
+      <h2 className="text-lg font-semibold text-grey-800 truncate">{product.name}</h2>
+      <p className="text-grey-600 font-medium">${product.price}</p>
+    </div>
+  )
+}
+
+export default ProductCard;
